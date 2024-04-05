3536b9a75a06a95c5d39c4534cb98c6dc28c53d7
Artem Kulakov <artem.kulakov@gmail.com>
2024-04-05 10:45:45 +0300

    Create items table

--- guestbook/resources/sql/queries.sql
+++ guestbook/resources/sql/queries.sql
@@ -7,3 +7,23 @@ VALUES (:name, :message)
 -- :name get-messages :? :*
 -- :doc selects all available messages
 SELECT * FROM guestbook
+
+
+
+
+
+-- :name get-items :? :*
+-- :doc selects all available items
+SELECT * FROM items
+
+-- :name add-item! :! :n
+-- :doc creates a new item
+INSERT INTO items
+(name)
+VALUES (:name)
+
+-- :name update-item-status! :! :n
+-- :doc updates the item status
+UPDATE items
+SET status = :status
+WHERE id = :id
