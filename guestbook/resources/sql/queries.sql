5d39934a7cc45f3b3799c7f33424f8c6c71c54bb
Artem Kulakov <artem.kulakov@gmail.com>
2024-05-14 10:46:53 +0300

    Style the page

--- guestbook/resources/sql/queries.sql
+++ guestbook/resources/sql/queries.sql
@@ -36,7 +36,7 @@ WHERE id = :id
 -- :name sort-items! :! :n
 -- :doc sorts the items and updates the values of the "sort" column
 WITH sorted_items AS (
-	SELECT id, ROW_NUMBER() OVER (ORDER BY complete DESC, timestamp ASC) AS new_sort FROM items
+	SELECT id, ROW_NUMBER() OVER (ORDER BY complete DESC, sort ASC) AS new_sort FROM items
 ) UPDATE items
 SET sort = sorted_items.new_sort
 FROM sorted_items
